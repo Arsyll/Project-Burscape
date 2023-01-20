3c3a1b37a893a4475774548f6d06d04f94cf4504
DhafinPxi <dhafinqintarakh@gmail.com>
2023-01-20 08:33:40 +0700

    First Push

---
+++ app/Models/FeedBack.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FeedBack extends Model
+{
+    use HasFactory;
+
+    protected $table = "feed_backs";
+
+    protected $fillable = [
+        'nama',
+        'email',
+        'subjek',
+        'pesan',
+    ];
+}
